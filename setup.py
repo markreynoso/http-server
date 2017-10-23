746a8ef799dd5528aba06582d99e44cc12568829
Mark Reynoso <mreynoso@spu.edu>
2017-10-23 14:51:15 -0700

    created environment and added base files

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+
+
+setup(
+    name='http-server',
+    description='A package for building and running the server module',
+    package_dir={'':'src'},
+    author='Mark and Brian',
+    author_email='mreynoso@spu.edu, monsteraono@gmail.com',
+    py_modules=['http-server'],
+    install_requires=[],
+    extras_require={
+        'testing': ['pytest', 'pytest-cov', 'pytest-watch', 'tox'],
+        'development': ['ipython']
+    },
+    # entry_points={
+    #     'console_scripts': [
+    #         'server=server:main'
+    #     ]
+    # }
+)
