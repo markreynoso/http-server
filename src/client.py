b4cab2a113fc8d3ad78b579897da378bed481bd5
Brian Wheeler <PoundsXD@localhost.localdomain>
2017-10-24 17:14:39 -0400

    Server now sends message back and remains open for further requests, will shut down when cntrl + c is used.

--- src/client.py
+++ src/client.py
@@ -2,8 +2,20 @@ import socket
 
 
 def client(message):
-    client = socket.socket(*socket.getaddrinfo('127.0.0.1', 5555)[1][:3])
-    client.connect(('127.0.0.1', 5555))
+    info = socket.getaddrinfo('127.0.0.1', 5558)
+    stream = [i for i in info if i[1] == socket.SOCK_STREAM][0]
+    client = socket.socket(*stream[:3])
+    client.connect((stream[-1]))
     client.sendall(message.encode('utf-8'))
+    message = ''
+    msg_recv = True
+    while msg_recv:
+        msg = client.recv(8)
+        message += msg
+        if len(msg) < 8:
+            msg_recv = False
+    print(message)
+    client.close()
+    
 
 # if __name__ == '__main__':
--- src/server.py
+++ src/server.py
@@ -1,15 +1,26 @@
 import socket
+import sys
 
 
 def server():
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
-    server.bind(('127.0.0.1', 5555))
+    server.bind(('127.0.0.1', 5558))
     server.listen(1)
-    conn, addr = server.accept()
-    message = ''
+    print('connected')
     while True:
-        msg = conn.recv(8)
-        message += msg
-        if len(msg) < 8:
-            break
-    print(message)
+        try:
+            conn, addr = server.accept()
+            message = ''
+            msg_recv = True
+            while msg_recv:
+                msg = conn.recv(8)
+                message += msg
+                if len(msg) < 8:
+                    msg_recv = False
+            conn.sendall(message)
+            print(message)
+        except KeyboardInterrupt:
+            print('Closing server')
+            conn.shutdown(socket.SHUT_WR)
+            conn.close()
+            sys.exit(0)
