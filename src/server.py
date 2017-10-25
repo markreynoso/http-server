58a9f6b34efa7dcdbbe032e00da65b5773bb962b
Mark Reynoso <mreynoso@spu.edu>
2017-10-25 13:02:08 -0700

    fixed non-ascii issue and string the length of buffer issue, added functionality from command line

--- src/server.py
+++ src/server.py
@@ -1,28 +1,38 @@
 # -*-coding=utf-8-*-
+"""Create a socket on which a server will run."""
 import socket
 import sys
 
 
 def server():
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
-    server.bind(('127.0.0.1', 5566))
+    """Create an open server to listen and echo message."""
+    server = socket.socket(
+        socket.AF_INET,
+        socket.SOCK_STREAM,
+        socket.IPPROTO_TCP
+    )
+    server.bind(('127.0.0.1', 5570))
     server.listen(1)
     print('connected')
-    while True:
-        try:
+    try:
+        while True:
             conn, addr = server.accept()
             message = b''
             msg_recv = True
             while msg_recv:
                 msg = conn.recv(8)
                 message += msg
-                if '|' in msg:
+                if b'|' in msg:
                     msg_recv = False
-            conn.sendall(message)
             message = message[:(len(message) - 1)]
-            print(message)
-        except KeyboardInterrupt:
-            print('Closing server')
-            conn.shutdown(socket.SHUT_WR)
+            print(message.decode('utf-8'))
+            message = message + '|'
+            conn.sendall(message)
             conn.close()
-            sys.exit(0)
+    except KeyboardInterrupt:
+        print('Closing server.')
+        server.close()
+
+
+if __name__ == '__main__':
+    server()
