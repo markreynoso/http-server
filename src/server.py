e3e5879943440afd1bd3b70948eac4bb77eba01c
Mark Reynoso <mreynoso@spu.edu>
2017-10-25 15:37:04 -0700

    started parse function

--- src/server.py
+++ src/server.py
@@ -16,7 +16,6 @@ def server():
     try:
         while True:
             conn, addr = server.accept()
-
             message = b''
             msg_recv = True
             while msg_recv:
@@ -40,10 +39,25 @@ def response_ok():
     return b'HTTP/1.1 200 OK \n <CRLF>\n'
 
 
-def response_error():
+def response_error(type):
     """Form a byte string for a 500 response."""
-    return b'HTTP/1.1 500 Internal server error \n <CRLF>\n'
+    return b'HTTP/1.1 {type} Internal server error \n <CRLF>\n'
 
+
+def parse_request(message):
+    """Parse the incoming request from client side"""
+    message_list = message.decode('utf-8').split('<CRLF>')
+    header_lines = message_[0].split('\n')
+    request_line = header_lines.pop(0).split()
+
+    if request_line[0] is 'GET' and request_line[2] is 'HTTP/1.1'
+        response_ok()
+    else:
+        response_error(405)
+    if check for header
+
+    if none of the above, return URI of request.
+        return request_list[1]
 
 if __name__ == '__main__':  # pragma: no cover
     server()
