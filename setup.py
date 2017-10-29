b0fdc9aa95a27af7972d0db9421effced415c22e
Mark Reynoso <mreynoso@spu.edu>
2017-10-29 15:50:34 -0700

    created branch and concurrent.py file

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
+"""Create appropriate environment for dev and use."""
 from setuptools import setup
-
 
 
 setup(
     name='http-server',
     description='A package for building and running the server module',
-    package_dir={'':'src'},
+    package_dir={'': 'src'},
     author='Mark and Brian',
     author_email='mreynoso@spu.edu, monsteraono@gmail.com',
     py_modules=['http-server'],
-    install_requires=[],
+    install_requires=['gevent'],
     extras_require={
         'testing': ['pytest', 'pytest-cov', 'pytest-watch', 'tox'],
         'development': ['ipython']
