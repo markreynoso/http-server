41ab2ed8f7f26ebcbccdf35954bd9edc209e54d5
Mark Reynoso <mreynoso@spu.edu>
2017-11-18 11:42:13 -0800

    fixed unused while loop and reindented block

--- src/concurrent.py
+++ src/concurrent.py
@@ -1,9 +1,11 @@
 # -*-coding=utf-8-*-
 """Create a socket on which a server will run."""
 import sys
-from gevent.server import StreamServer
+
 from gevent.monkey import patch_all
-from server import response_ok, response_error, parse_request, resolve_uri
+from gevent.server import StreamServer
+
+from server import parse_request, resolve_uri, response_error, response_ok
 
 
 def server():  # pragma: no cover
@@ -21,31 +23,30 @@ def server():  # pragma: no cover
 
 def echo(socket, addr):  # pragma: no cover
     """Receive and return client messages."""
-    while True:
-        message = b''
-        msg_recv = True
-        while msg_recv:
-            msg = socket.recv(8)
-            message += msg
-            if b'|' in msg:
-                msg_recv = False
-        message = message[:(len(message) - 1)]
+    message = b''
+    msg_recv = True
+    while msg_recv:
+        msg = socket.recv(8)
+        message += msg
+        if b'|' in msg:
+            msg_recv = False
+    message = message[:(len(message) - 1)]
+    try:
+        response = parse_request(message)
         try:
-            response = parse_request(message)
-            try:
-                return_search = resolve_uri(response)
-                ok = response_ok(return_search)
-                message = ok + '|'
-                socket.sendall(message.encode('utf-8'))
-            except ValueError as back:
-                error_msg = response_error(back)
-                message = error_msg + '|'
-                socket.sendall(message.encode('utf-8'))
+            return_search = resolve_uri(response)
+            ok = response_ok(return_search)
+            message = ok + '|'
+            socket.sendall(message.encode('utf-8'))
         except ValueError as back:
             error_msg = response_error(back)
             message = error_msg + '|'
             socket.sendall(message.encode('utf-8'))
-        socket.close()
+    except ValueError as back:
+        error_msg = response_error(back)
+        message = error_msg + '|'
+        socket.sendall(message.encode('utf-8'))
+    socket.close()
 
 
 if __name__ == '__main__':  # pragma: no cover
